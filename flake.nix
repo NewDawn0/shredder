687ef3199daef15a942b0bf12c72162c76c5f7e8
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-13 15:01:25 +0100

    use nixUtils

--- flake.nix
+++ flake.nix
@@ -1,37 +1,30 @@
 {
-  description = "File deletion for the paranoid ones";
+  description = "Fast file deletion for the paranoid ones";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    nix-systems.url = "github:nix-systems/default";
-  };
+  inputs.utils.url = "github:NewDawn0/nixUtils";
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    let eachSystem = nixpkgs.lib.genAttrs (import inputs.nix-systems);
-    in {
-      overlays.default =
-        (final: prev: { note = self.packages.${prev.system}.default; });
-      packages = eachSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          default = pkgs.buildGoModule {
-            pname = "shredder";
-            version = "1.0.0";
-            src = ./.;
-            vendorHash = null;
-            meta = {
-              description =
-                "A tool for securely deleting files to ensure privacy";
-              longDescription = ''
-                This tool securely deletes files, ensuring they cannot be recovered.
-                Ideal for users who require extra privacy and need to ensure sensitive data is completely erased from their system.
-              '';
-              homepage = "https://github.com/NewDawn0/shredder";
-              license = pkgs.lib.licenses.mit;
-              maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
-              platforms = pkgs.lib.platforms.all;
-            };
-          };
-        });
+  outputs = { self, utils, ... }: {
+    overlays.default = final: prev: {
+      shredder = self.packages.${prev.system}.default;
     };
+    packages = utils.lib.eachSystem { } (pkgs: {
+      default = pkgs.buildGoModule {
+        pname = "shredder";
+        version = "1.0.0";
+        src = ./.;
+        vendorHash = null;
+        meta = {
+          description = "File deletion for the paranoid ones";
+          longDescription = ''
+            "A tool for securely deleting files to ensure privacy";
+            Ideal for users who require extra privacy and need to ensure sensitive data is completely erased from their system.
+          '';
+          homepage = "https://github.com/NewDawn0/shredder";
+          license = pkgs.lib.licenses.mit;
+          maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
+          platforms = pkgs.lib.platforms.all;
+        };
+      };
+    });
+  };
 }
